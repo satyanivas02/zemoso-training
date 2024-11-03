b1c3463b2030612d3e6a972afe142bcdae23fa42
satyanivas02 <satyanivas.miryala@zemosolabs.com>
2024-11-03 21:21:54 +0530

    Added react webpack assignment

---
+++ react-webpack/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+export const App = () => {
+  return (
+    <div >
+      <h1>React TypeScript Webpack</h1>
+      <h1>Range-Rover-Velar</h1>
+      <img src="assets/range_rover.jpg" alt="Range-Rover-Velar" /> 
+    </div>
+  );
+};
