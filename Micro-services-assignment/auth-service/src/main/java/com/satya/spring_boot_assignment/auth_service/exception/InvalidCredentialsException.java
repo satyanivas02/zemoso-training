5bc437c94d51e6b8c3caf8c2b7d6e4cfaa0e3524
satyanivas02 <satyanivas.miryala@zemosolabs.com>
2025-01-07 12:52:23 +0530

    Delete Micro-services-assignment/auth-service directory
    recommended to delete

--- Micro-services-assignment/auth-service/src/main/java/com/satya/spring_boot_assignment/auth_service/exception/InvalidCredentialsException.java
+++
@@ -1,7 +0,0 @@
-package com.satya.spring_boot_assignment.auth_service.exception;
-
-public class InvalidCredentialsException extends RuntimeException {
-    public InvalidCredentialsException(String message) {
-        super(message);
-    }
-}
